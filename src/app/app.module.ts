0f541a4b8018583cc0c54b16915d1f9757df0b7c
Azeritzh <azhlander@outlook.com>
2019-01-14 15:57:29 +0100

    Cleaned up in styling sheets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { AboutpageComponent } from './aboutpage/aboutpage.component';
 import { SkillpageComponent } from './skillpage/skillpage.component';
+import { ProjectpageComponent } from './projectpage/projectpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
     AboutpageComponent,
-    SkillpageComponent
+    SkillpageComponent,
+    ProjectpageComponent
   ],
   imports: [
     BrowserModule,
