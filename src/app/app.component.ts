a6d8f0be1a6bb35a6219ef269b38df0b18c953aa
Azeritzh <azhlander@outlook.com>
2019-01-18 15:49:00 +0100

    Added thesis description and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,18 +25,18 @@ export class AppComponent {
       background: ""
     },
     {
-      title: "Private Healthcare Webapplication",
-      technologies: "Angular, TypeScript, HTML, SCSS, C#, NodeJS, Jasmine",
+      title: "Master Thesis",
+      technologies: "Unity, VR technology (Oculus Rift), Hand-motion sensor (Leap Motion)",
       texts: [
-        "I've worked on this project as a unpaid part-time job from back in 2015, where it started, until now. My roles in this project was Frontend developer and UI Designer.",
-        "I've coded on serveral modules throughout the process, where I, among others, have been working on the language and patient journal module.",
-        "My main task was to design the overall look of the application and make sure every page would be easily accessible for new users.",
-        "To the left you can see images of the application as well as the template system for the journal module. The project is still on-going to this day."],
+        "I finished my master thesis back in July 2018 with a fellow student. The project involved rehabilitation of neurological patients through stimulation of the mirror neurons in the brain through VR. ",
+        "The whole project was coded in Unity and incorporated the Oculus Rift as sensor and the Leap Motion as actuator for the patient.",
+        "To stimulate the mirror neurons in the brain we made a virtual hand in VR that was controlled by the healthy hand. This could trick the brain into thinking that the hand was the patients own.",
+        "Through an agile iterative proces where each prototype were tested with healthy persons, the project concluded with a fully functional prototype with two exercises to train hand coordination."],
       images: [
-        "assets/images/idun-start.png",
-        "assets/images/idun-login.png",
-        "assets/images/idun-template.png",
-        "assets/images/idun-patient.png"],
+        "assets/images/thesis-task.png",
+        "assets/images/thesis-demonstration.png",
+        "assets/images/thesis-unity.png",
+        "assets/images/thesis-code.png"],
         background: "true"
     }
   ]
