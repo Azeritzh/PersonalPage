d62bd44858762ec55e7af6c1fdb4ba4170ae0ae3
Azeritzh <azhlander@outlook.com>
2019-07-10 16:27:46 +0200

    Fixed scroll navigation and added color img

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-menu',
@@ -6,15 +6,30 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./navigation-menu.component.scss']
 })
 export class NavigationMenuComponent implements OnInit {
+  @ViewChild('pageContent') pageContent: ElementRef
 
   constructor() { }
 
   ngOnInit() {
+    document.getElementById('mainContainer').addEventListener('scroll', function(e) {
+      console.log(document.getElementById('mainContainer').scrollTop)
+    })
   }
 
-  scrollToElement(element) {
-    document.getElementById(element).scrollIntoView({behavior: "smooth"})
-    document.getElementById('navbar').style.display = 'table'
+  scrollToComponent(element) {
+    let mainComponent = document.getElementById('mainContainer')
+    switch(element) {
+      case 'about':
+        mainComponent.scrollTop = 1000
+        break
+      case 'portefolio':
+        mainComponent.scrollTop = 2500
+        break
+      case 'services':
+        mainComponent.scrollTop = 3300
+        break
+      case 'skills':
+        mainComponent.scrollTop = 4000
+    }
   }
-
 }
